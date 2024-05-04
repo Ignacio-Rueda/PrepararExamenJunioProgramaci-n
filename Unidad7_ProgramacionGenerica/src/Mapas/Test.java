61d1ede9b2b8c821820e25794fd64c4f4158431a
Ignacio-Rueda <nachord@outlook.com>
2024-05-04 07:25:43 +0200

    Examen junio.

--- Unidad7_ProgramacionGenerica/src/Mapas/Test.java
+++ Unidad7_ProgramacionGenerica/src/Mapas/Test.java
@@ -3,6 +3,7 @@ package Mapas;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class Test {
     
@@ -12,8 +13,14 @@ public class Test {
         mapa.put("01", new Empleado("Raúl",33,3500));
         mapa.put("02",new Empleado("Sandra",27,2900));
         mapa.put("03",new Empleado("Isabel",28,3600));
-    
-        
+        mapa.remove("01");
+        //Para reemplazarlo, introducir el elemento con la misma clave
+        mapa.put("02", new Empleado("Alejandro",24,2700));
+        Set<Map.Entry<String,Empleado>> s = mapa.entrySet();
+        for(Map.Entry<String,Empleado> i:s){
+            System.out.println(i.getKey());
+            System.out.println(i.getValue().getEdad());
+        }
         
     }
     
