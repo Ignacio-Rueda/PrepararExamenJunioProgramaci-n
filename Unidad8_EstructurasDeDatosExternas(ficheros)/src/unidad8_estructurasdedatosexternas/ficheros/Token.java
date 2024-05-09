620a0eb4d501acba5c8b86c79100ff3de8f98b78
Ignacio-Rueda <nachord@outlook.com>
2024-05-09 18:08:58 +0200

    Examen junio

---
+++ Unidad8_EstructurasDeDatosExternas(ficheros)/src/unidad8_estructurasdedatosexternas/ficheros/Token.java
@@ -0,0 +1,39 @@
+package unidad8_estructurasdedatosexternas.ficheros;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.StreamTokenizer;
+
+public class Token {
+
+    public void contarPlabrasYNumeros(String nombreFichero) {
+        StreamTokenizer sTokenizer = null;
+        int contadorPalabras = 0;
+        int numeroDenumeros = 0;
+        try {
+            sTokenizer = new StreamTokenizer(new FileReader(""));
+            while (sTokenizer.nextToken() != StreamTokenizer.TT_EOF) {
+                if (sTokenizer.ttype == StreamTokenizer.TT_WORD) {
+                    contadorPalabras++;
+                } else if (sTokenizer.ttype == StreamTokenizer.TT_NUMBER) {
+                    numeroDenumeros++;
+                }
+            }
+            System.out.println("Número de palabras en el fichero:" + contadorPalabras);
+            System.out.println("Número de números en el fichero:" + numeroDenumeros);
+        } catch (FileNotFoundException ex) {
+            System.out.println("No se ha encontrado el fichero"+ex.getMessage());
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        new Token().contarPlabrasYNumeros("C:\\ficheros\\datos.txt");
+       
+
+    }
+
+}
