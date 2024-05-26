2eb4ad4ce17000cd39a23c8d8cbccb7c7d4895e9
Ignacio Rueda <nachord@outlook.com>
2024-05-26 12:09:02 +0200

    Atributos de clase y objeto.

---
+++ UtilidadesYejemplos/src/utilidadesyejemplos/Vehiculo.java
@@ -0,0 +1,10 @@
+
+package utilidadesyejemplos;
+
+
+public class Vehiculo {
+    /**
+     * Lo recomendable es que los atributos lleven el modificador -> private.
+     * Si se quiere acceder o manipular que se tenga que hacer a través de los métodos de la clase
+    */    
+}
